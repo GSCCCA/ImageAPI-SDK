a0fd0721635a535e1d74705491257d5a4076f723
Matt Bommicino <matt.bommicino@gsccca.org>
2020-06-16 14:34:09 -0400

    Changes to project file to easily create nuget package

---
+++ GSCCCA.ImageAPI.Client/ApiObjects/ImageUploadResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GSCCCA.ImageAPI.Client.ApiObjects
+{
+    public class ImageUploadResult
+    {
+        [JsonProperty("batchCode")]
+        public string BatchCode { get; set; }
+
+        [JsonProperty("filename")]
+        public string Filename { get; set; }
+
+        [JsonProperty("imageId")]
+        public long ImageId { get; set; }
+
+        [JsonProperty("isValidated")]
+        public bool IsValidated { get; set; }
+
+        [JsonProperty("reason")]
+        public string Reason { get; set; }
+    }
+}
